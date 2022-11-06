8fbd2ac83ec8a91af03f1ce6ae1f2f63430bae2b
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-11-05 23:08:46 -0300

    menu left em desenvolvimento

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const feature = document.querySelector("#features")
 const company = document.querySelector("#company")
 
-feature.addEventListener("click", () =>{
+feature.addEventListener("click", () => {
     let first = document.querySelector(".first")
     let arrow = document.querySelector(".arrow-down")
     if (feature){
@@ -11,12 +11,31 @@ feature.addEventListener("click", () =>{
     }
 })
 
-company.addEventListener("click", () =>{
+company.addEventListener("click", () => {
     let secund = document.querySelector(".secund")
     let arrow2 = document.querySelector(".arrow-down2")
 
     if (company) {
         secund.classList.toggle("active")
         arrow2.classList.toggle("rotate")
+    }
+})
+
+const menu = document.querySelector(".menu-i")
+const x = document.querySelector(".x")
+
+menu.addEventListener("click", () => {
+    
+    if (menu) {
+        menu.style.display = "none"
+        x.style.display = "flex"
+    }
+})
+
+x.addEventListener("click", () => {
+
+    if(x){
+        menu.style.display = "flex"
+        x.style.display = "none"
     }
 })
