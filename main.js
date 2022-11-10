4ecf0908857efc8fa0adf88b2af1f295d3d151ab
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-11-09 22:53:31 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,31 +1,57 @@
-//variables
+//variables//
 const feature = document.querySelector("#features")
 const company = document.querySelector("#company")
 
+//functions//
 feature.addEventListener("click", () => {
     let first = document.querySelector(".first")
-    let arrow = document.querySelector(".arrow-down")
+    let arrowF = document.querySelector(".arrow-down-features")
     if (feature){
         first.classList.toggle("active")
-        arrow.classList.toggle("rotate")
+        arrowF.classList.toggle("rotate")
     }
 })
 
 company.addEventListener("click", () => {
     let secund = document.querySelector(".secund")
-    let arrow2 = document.querySelector(".arrow-down2")
+    let arrowC = document.querySelector(".arrow-down-company")
 
     if (company) {
         secund.classList.toggle("active")
+        arrowC.classList.toggle("rotate")
+    }
+})
+
+//variables//
+const menu = document.querySelector(".menu-i")
+const x = document.querySelector(".x")
+const left = document.querySelector(".menu-left")
+const menuLeftFirst = document.querySelector(".menu-left-first")
+const menuLeftSecund = document.querySelector(".menu-left-a")
+const show = document.querySelector(".show")
+const show2 = document.querySelector(".show2")
+
+
+//functions//
+menuLeftFirst.addEventListener("click", () => {
+    let arrow2 = document.querySelector(".arrow-down")
+    if (menuLeftFirst) {
+        show.classList.toggle("active1")
+        arrow2.classList.toggle("rotate")
+    }
+       
+})
+
+menuLeftSecund.addEventListener("click", () => {
+    let arrow2 = document.querySelector(".arrow-down2")
+
+    if(menuLeftSecund) {
+        show2.classList.toggle("active1")
         arrow2.classList.toggle("rotate")
     }
 })
 
-const menu = document.querySelector(".menu-i")
-const x = document.querySelector(".x")
-const left = document.querySelector(".menu-left")
-
-
+//functions//
 menu.addEventListener("click", () => {
     
     if (menu) {
@@ -43,3 +69,4 @@ x.addEventListener("click", () => {
         left.style.display = "none"
     }
 })
+
